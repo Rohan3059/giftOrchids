2604e0e0702eda0b9f5fae5394a27cea4bc412fd
[kRavi07] <[kantravi163@gmail.com]>
2024-05-17 20:56:44 +0530

    Added previous period counts and percentage changes to analytics response, and introduced new functions

--- controllers/analytics.go
+++ controllers/analytics.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"context"
+	"log"
 	"net/http"
 	"strconv"
 	"time"
@@ -95,27 +96,55 @@ func GetAnalytics() gin.HandlerFunc {
 			return
 		}
 
-		response := gin.H{
+		// Previous period counts
+		previousUserCount, err := CountDocumentInRange(UserCollection, ctx, time.Now().AddDate(0, 0, -2*int(daysInt)), time.Now().AddDate(0, 0, -int(daysInt)))
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"Error": err.Error()})
+			return
+		}
 
+		previousSellerCount, err := CountDocumentInRange(SellerCollection, ctx, time.Now().AddDate(0, 0, -2*int(daysInt)), time.Now().AddDate(0, 0, -int(daysInt)))
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"Error": err.Error()})
+			return
+		}
+
+		previousEnquiryCount, err := CountDocumentInRange(EnquireCollection, ctx, time.Now().AddDate(0, 0, -2*int(daysInt)), time.Now().AddDate(0, 0, -int(daysInt)))
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"Error": err.Error()})
+			return
+		}
+
+		// Calculate percentage changes
+		userPercentageChange := CalculatePercentageChange(user_count, previousUserCount)
+		sellerPercentageChange := CalculatePercentageChange(sellerCount, previousSellerCount)
+		enquiryPercentageChange := CalculatePercentageChange(enquiry_count, previousEnquiryCount)
+
+		response := gin.H{
 			"users": gin.H{
-				"count":         user_count,
-				"one_day_count": user_one_day_count,
-				"total":         total_user_count,
+				"count":             user_count,
+				"one_day_count":     user_one_day_count,
+				"total":             total_user_count,
+				"percentage_change": userPercentageChange,
+				"previous_count":    previousUserCount,
 			},
 			"sellers": gin.H{
-				"count":         sellerCount,
-				"one_day_count": seller_one_day_count,
-				"total":         seller_total_count,
+				"count":             sellerCount,
+				"one_day_count":     seller_one_day_count,
+				"total":             seller_total_count,
+				"percentage_change": sellerPercentageChange,
+				"previous_count":    previousSellerCount,
 			},
 			"enquiries": gin.H{
-				"count":         enquiry_count,
-				"one_day_count": enquiry_one_day_count,
-				"total":         total_enquiry_count,
+				"count":             enquiry_count,
+				"one_day_count":     enquiry_one_day_count,
+				"total":             total_enquiry_count,
+				"percentage_change": enquiryPercentageChange,
+				"previous_count":    previousEnquiryCount,
 			},
 		}
 
 		c.JSON(http.StatusOK, response)
-
 	}
 }
 
@@ -135,4 +164,44 @@ func CountDocument(collection *mongo.Collection, ctx context.Context, days int) 
 
 	count, err := collection.CountDocuments(ctx, filter)
 	return count, err
+}
+
+func CountDocumentInRange(
+	collection *mongo.Collection,
+	ctx context.Context,
+	startDate time.Time,
+	endDate time.Time,
+) (int64, error) {
+	endDate = endDate.Add(23*time.Hour + 59*time.Minute + 59*time.Second + 999*time.Millisecond)
+
+	filter := bson.M{
+
+		"created_at": bson.M{
+
+			"$gte": primitive.NewDateTimeFromTime(startDate),
+
+			"$lte": primitive.NewDateTimeFromTime(endDate),
+		},
+	}
+
+	count, err := collection.CountDocuments(ctx, filter)
+
+	if err != nil {
+
+		log.Fatal(err)
+
+	}
+
+	return count, nil
+}
+
+func CalculatePercentageChange(currentCount, previousCount int64) float64 {
+	if previousCount == 0 {
+
+		if currentCount > 0 {
+			return 100.0
+		}
+		return 0.0
+	}
+	return (float64(currentCount-previousCount) / float64(previousCount)) * 100
 }
